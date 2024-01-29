7a205c1491f141bfe34d77e55b216b8419d7db93
Rafael Cerqueira <rafaelcerqueiraf@outlook.com>
2024-01-29 15:41:26 -0300

    adicionando campo email no formulario

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 function validarNome() {
     let nome = document.getElementById("input-nome").value;
     let spanNome = document.getElementById("span__nome");
+
     if (nome == "") {
         spanNome.style.visibility = "visible";
     } else {
@@ -9,16 +10,55 @@ function validarNome() {
     
 }
 
+function validarEmail() {
+    let email = document.getElementById("input-email").value;
+    let spanEmail = document.getElementById("span__email");
+
+    if (email == "") {
+        spanEmail.style.visibility = "visible";
+    } else {
+        spanEmail.style.visibility = "hidden";
+    }
+}
+
 function validarTelefone() {
     let telefone = document.getElementById("input-telefone").value;
-    let spanTelefone = document.getElementById("span__telefone");
+    let numero = telefone.replace(/\D/g, "");
+    //let spanTelefone = document.getElementById("span__telefone");
+
+
+    /*
     if (telefone == "") {
+        spanTelefone.style.visibility = "visible";
+    } else {
+        spanTelefone.style.visibility = "hidden";
+    }
+    */
+
+    if (numero.length === 11) {
+        spanTelefone.textContent = "Telefone inválido";
         spanTelefone.style.visibility = "visible";
     } else {
         spanTelefone.style.visibility = "hidden";
     }
     
 }
+
+function formatarTelefone(input) {
+    let numero = input.value.replace(/\D/g, "");
+
+    if (numero.length > 1) {
+        numero = `(${numero.substring(0, 2)}) ${numero.substring(2)}`;
+    }
+
+    if (numero.length > 10) {
+        numero = `${numero.substring(0, 10)}-${numero.substring(10, 14)}`;
+    }
+
+    input.value = numero;
+
+}
+
 
 function validarMensagem() {
     let mensagem = document.getElementById("input-mensagem").value;
@@ -34,6 +74,7 @@ function validarMensagem() {
 
 function limparFormulario() {
     document.getElementById("input-nome").value = "";
+    document.getElementById("input-email").value = "";
     document.getElementById("input-telefone").value = "";
     document.getElementById("input-mensagem").value = "";
 }
@@ -42,11 +83,17 @@ function limparFormulario() {
 function confirmarEnvio() {
 
     let nome = document.getElementById("input-nome").value.trim();
-    let telefone = document.getElementById("input-telefone").value.trim();
     let mensagem = document.getElementById("input-mensagem").value.trim();
+    let email = document.getElementById("input-email").value.trim();
 
-    if (nome === "" || telefone === "" || mensagem === "") {
+    if (nome === "" || email === "" || mensagem === "") {
+
         exibirMensagem("Preencha todos os campos!", "red");
+
+        document.getElementById("input-nome").style.border = "2px solid red";
+        document.getElementById("input-email").style.border = "2px solid red";
+        document.getElementById("input-mensagem").style.border = "2px solid red";
+
     } else {
 
         exibirMensagem("Mensagem enviada com sucesso!", "green");
@@ -55,6 +102,10 @@ function confirmarEnvio() {
     }
 
     return false;
+}
+
+function removerBorda(elemento) {
+    elemento.style.border = "none";
 }
 
 function exibirMensagem(texto, cor) {
